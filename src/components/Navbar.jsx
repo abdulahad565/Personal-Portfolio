4037c0c876daf7cfd4f66166e6989e8d7fb9fd27
abdulahad565 <aahad23p@gmail.com>
2024-09-25 22:33:16 +0500

    Describe your changes here

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <h1>Abdul Ahad Khan</h1>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/skills">Skills</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      <h1 className="navbar-logo">Abdul Ahad Khan</h1>
+      <div className="menu-icon" onClick={toggleMobileMenu}>
+        <i className={isMobileMenuOpen ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={isMobileMenuOpen ? "nav-links active" : "nav-links"}>
+        <li><Link to="/" onClick={toggleMobileMenu}>Home</Link></li>
+        <li><Link to="/about" onClick={toggleMobileMenu}>About</Link></li>
+        <li><Link to="/projects" onClick={toggleMobileMenu}>Projects</Link></li>
+        <li><Link to="/skills" onClick={toggleMobileMenu}>Skills</Link></li>
+        <li><Link to="/contact" onClick={toggleMobileMenu}>Contact</Link></li>
       </ul>
     </nav>
   );
